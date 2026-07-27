2cb6264ca1630ae594cf91795d8980a4e31b89ae
Joonas Salonpää <joonas1000@gmail.com>
2026-07-27 12:44:30 -0400

    multiwt: Tweaks
    Drop LFO waveform parameter and replace it with LFO target.
    Now shape LFO always targets waveform selection but the additional
    LFO can target also window or position. The modulation can be
    a bit unpredictable especially with the sticky parameter but
    it improves the sonic capabilities a lot.

--- multiwt/README.md
+++ multiwt/README.md
@@ -15,18 +15,21 @@ using it. It comes with an extra LFO that can be used for additional shape modul
     * Wavetable window (0 % = full wavetable, 100 % = 5 % of the length (~6 samples))
 
 #### User parameters
-- 1: Wavetable playback position
+- 1: LFO intensity
+- 2: LFO frequency (0.01...1.01 Hz)
+- 3: LFO target, bitmask (1..7):
+	- 001 = Wavetable window
+	- 010 = Wavetable position
+	- 100 = Wavetable selection
+- 4: Stickiness
+    * Affects how long the engine holds one wavetable before switching.
+    * Range: 0 % = immediate, 100 % = 1 second delay
+- 5: Wavetable playback position
     * Has a bigger impact with smaller wavetable windows. The looped waveform is in range:
       position..position+window. Window is a ratio of total wavetable length and the position
       controls the playback starting position in the remainder of the wavetable. (The start position
       will not wrap, so e.g. with window=0.2, the position will be in range 0..0.8).
-- 2: Wavetable ordering
-- 3: LFO intensity
-- 4: LFO frequency (0.01...1.01 Hz)
-- 5: LFO waveform (triangle, sine, sawtooth, square)
-- 6: Stickiness
-    * Affects how long the engine holds one wavetable before switching.
-    * Range: 0 % = immediate, 100 % = 1 second delay
+- 6: Wavetable ordering
 
 ## License
 Original code MIT licensed (see LICENSE.md). Korg code BSD 3-Clause Licensed, license headers retained in relevant code files.
--- multiwt/osc.c
+++ multiwt/osc.c
@@ -1,7 +1,8 @@
-#include "userosc.h"
+﻿#include "userosc.h"
 #include "basic_oscillator.h"
 #include "waveforms.h"
 #include "waveform_random_orders.h"
+#include "manifest_params.h"
 
 static BasicOscillator osc, osc2, lfo;
 static float window = 1, pos = 0, wt_param = 0;
@@ -10,13 +11,19 @@ static enum OscType lfo_type = OSC_TRIANGLE;
 static float lfo_int = 0;
 static uint32_t sticky = 0, sticky_counter = 0;
 
+#define LFO_TARGET_WIN 1
+#define LFO_TARGET_POS 2
+#define LFO_TARGET_WF 4
+#define LFO_TARGET_ALL 7
+static uint8_t lfo_target = LFO_TARGET_WIN;
+
 static inline void update_wt(BasicOscillator *o, float selection)
 {
-	int isel = selection * NUMBER_OF_WAVEFORMS;
-	while (isel >= NUMBER_OF_WAVEFORMS) isel -= NUMBER_OF_WAVEFORMS;
-	while (isel < 0) isel += NUMBER_OF_WAVEFORMS;
-	isel = get_index_mapping(ordering, isel);
-	BasicOscillator_setWavetable(o, (float*)get_waveform(isel), WAVEFORM_LENGTH);
+    int isel = selection * NUMBER_OF_WAVEFORMS;
+    while (isel >= NUMBER_OF_WAVEFORMS) isel -= NUMBER_OF_WAVEFORMS;
+    while (isel < 0) isel += NUMBER_OF_WAVEFORMS;
+    isel = get_index_mapping(ordering, isel);
+    BasicOscillator_setWavetable(o, (float*)get_waveform(isel), WAVEFORM_LENGTH);
 }
 
 void OSC_INIT(uint32_t platform, uint32_t api)
@@ -46,11 +53,29 @@ void OSC_CYCLE(const user_osc_param_t *const params,
   // starts as a carbon copy of osc.
   memcpy(&osc2, &osc, sizeof(osc));
   const float lfo_val = BasicOscillator_getValue(&lfo, lfo_type) * lfo_int;
+
+  float mod_pos = pos, mod_window = window;
   if (sticky_counter >= sticky)
   {
-	update_wt(&osc, wt_param + shape_lfo + lfo_val);
-	sticky_counter = 0;
+    if (lfo_target & LFO_TARGET_WF)
+        update_wt(&osc, wt_param + shape_lfo + lfo_val);
+    else
+        update_wt(&osc, wt_param + shape_lfo);
+    sticky_counter = 0;
   }
+  if (lfo_target & LFO_TARGET_POS)
+  {
+      mod_pos += lfo_val;
+      while (mod_pos > 1) mod_pos -= 1;
+      while (mod_pos < 0) mod_pos += 1;
+  }
+  if (lfo_target & LFO_TARGET_WIN)
+  {
+      mod_window += lfo_val;
+      while (mod_window > 1) mod_window -= 1;
+      while (mod_window < 0) mod_window += 1;
+  }
+  BasicOscillator_setWaveTableParams(&osc, mod_pos, mod_window);
 
   q31_t *__restrict y = (q31_t *)yn;
   const q31_t *y_e = y + frames;
@@ -59,13 +84,13 @@ void OSC_CYCLE(const user_osc_param_t *const params,
 
   for (; y != y_e;)
   {
-	BasicOscillator_calculateNext(&osc);
-	BasicOscillator_calculateNext(&osc2);
-	BasicOscillator_calculateNext(&lfo);
+    BasicOscillator_calculateNext(&osc);
+    BasicOscillator_calculateNext(&osc2);
+    BasicOscillator_calculateNext(&lfo);
     const float o1 = BasicOscillator_getValue(&osc, OSC_WT);
     const float o2 = BasicOscillator_getValue(&osc2, OSC_WT);
     mix += mix_inc;
-	
+
     const float out = o1 * mix + o2 * (1 - mix);
     *(y++) = f32_to_q31(out);
   }
@@ -86,42 +111,33 @@ void OSC_PARAM(uint16_t index, uint16_t value)
 {
   switch (index)
   {
-  case k_user_osc_param_id1:
+  case USER_PARAM__Position__idx:
     pos = value / 100.0;
-    BasicOscillator_setWaveTableParams(&osc, pos, window);
     break;
-  case k_user_osc_param_id2:
+  case USER_PARAM__Order__idx:
     ordering = value % ORDER_MAP_SIZE;
     break;
-  case k_user_osc_param_id3:
+  case USER_PARAM__LFO_Int__idx:
     lfo_int = value / 100.0;
     break;
-  case k_user_osc_param_id4:
+  case USER_PARAM__LFO_Freq__idx:
     BasicOscillator_setFrequency(&lfo, (value + 1) / 100.0 / k_samplerate);
     break;
-  case k_user_osc_param_id5:
-    if (value == 0)
-		lfo_type = OSC_TRIANGLE;
-    else if (value == 1)
-		lfo_type = OSC_SINE;
-    else if (value == 2)
-		lfo_type = OSC_SAW;
-    else if (value == 3)
-		lfo_type = OSC_SQUARE;
+  case USER_PARAM__LFO_Target__idx:
+    lfo_target = (value + 1) & LFO_TARGET_ALL;
     break;
-  case k_user_osc_param_id6:
+  case USER_PARAM__Sticky__idx:
     sticky = k_samplerate / 100 * value;
     break;
   case k_user_osc_param_shape:
-	{
-		wt_param = param_val_to_f32(value) * 0.99;
-	}
+    {
+        wt_param = param_val_to_f32(value) * 0.99;
+    }
     break;
   case k_user_osc_param_shiftshape:
-	{
-		window =  0.05 + 0.95 * (1 - param_val_to_f32(value));
-		BasicOscillator_setWaveTableParams(&osc, pos, window);
-	}
+    {
+        window =  0.05 + 0.95 * (1 - param_val_to_f32(value));
+    }
     break;
   default:
     break;
