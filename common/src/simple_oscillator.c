e97b1c48d4db82dd93f1ef23f2127df5f59d5dc3
Joonas Salonpää <joonas1000@gmail.com>
2026-07-29 19:17:43 -0400

    common/simple_oscillator: Create stripped down version of basic_oscillator
    For usecases where more sophisticated features are not needed or
    don't fit to memory.

---
+++ common/src/simple_oscillator.c
@@ -0,0 +1,45 @@
+#include "simple_oscillator.h"
+
+inline static float tri1(float phase)
+{
+    if (phase < 0.5)
+        return 4 * phase - 1;
+    else
+        return -4 * phase + 3;
+}
+inline static float saw1(float phase)
+{
+    return 2 * phase - 1;
+}
+inline static float sqr1(float phase)
+{
+    return phase < 0.5 ? 1.0f : -1.0f;
+}
+
+void SimpleOscillator_calculateNext(SimpleOscillator* so)
+{
+    so->phase = so->phase + so->frequency;
+    if (so->phase >= 1.0)
+    {
+        so->phase = so->phase - 1.0f;
+    }
+}
+
+inline static float get_value(float p, enum OscType oscType)
+{
+    switch (oscType)
+    {
+    case OSC_TRIANGLE:
+        return tri1(p);
+    case OSC_SAW:
+        return saw1(p);
+    case OSC_SQUARE:
+    default:
+        return sqr1(p);
+    }
+}
+
+float SimpleOscillator_getValue(SimpleOscillator* so, enum OscType oscType)
+{
+    return get_value(so->phase, oscType);
+}
---
+++ common/src/simple_oscillator.h
@@ -0,0 +1,32 @@
+#pragma once
+
+// Stripped down version of basic_oscillator
+
+// Oscillator waveform types
+enum OscType
+{
+    OSC_TRIANGLE,
+    OSC_SAW,
+    OSC_SQUARE
+};
+
+/*
+ * An oscillator for music syntheziser use. Has the following waveforms:
+ * triangle wave, saw wave, square wave
+ */
+typedef struct
+{
+    // Current phase (0...1)
+    float phase;
+    // Phase increment: this is added to the phase on each sample
+    float frequency;
+} SimpleOscillator;
+
+/*
+ * Proceeds the state by one sample. Get the signal amplitude value using the SimpleOscillator_getValue function.
+ */
+void SimpleOscillator_calculateNext(SimpleOscillator *so);
+/*
+ * Get current oscillator signal value for the given oscillator type.
+ */
+float SimpleOscillator_getValue(SimpleOscillator *so, enum OscType oscType);
