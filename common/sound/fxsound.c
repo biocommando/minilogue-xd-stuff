8bec79ec700b9a2d86c9ac1f3e4cf4b87a975027
Joonas Salonpää <joonas1000@gmail.com>
2026-08-13 14:18:24 +0000

    common/sound/fxsound.c: Add a sound tool also for modfx
    Add similar tool for modfx as there is for oscs.
    clip is the only project currently supporting the new tool.

---
+++ common/sound/fxsound.c
@@ -0,0 +1,75 @@
+#include "../wav_handler/wav_handler.h"
+
+#include "modfx_api.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <math.h>
+
+#define LIMQ31(x) \
+  x = x >= 0.99999997f ? 0.99999997f : x
+
+int main(int argc, char **argv)
+{  
+  if (argc < 5)
+  {
+    puts("Required arguments:");
+    puts("input wav");
+    puts("output wav");
+    puts("time param");
+    puts("depth param");
+    return 1;
+  }
+  
+  struct wav_file w_in, w_out;
+  if (read_wav_file(argv[1], &w_in) != 0)
+  {
+    puts("Can't read input wav file");
+    return 1;
+  }
+  create_wav_file(&w_out, w_in.num_frames, 2, 16, k_samplerate);
+  
+  MODFX_INIT(0, 0);
+  
+  float p_time = 0, p_depth = 0;
+  sscanf(argv[3], "%f", &p_time);
+  sscanf(argv[4], "%f", &p_depth);
+  LIMQ31(p_time);
+  LIMQ31(p_depth);
+  MODFX_PARAM(k_user_modfx_param_time, f32_to_q31(p_time));
+  MODFX_PARAM(k_user_modfx_param_depth, f32_to_q31(p_depth));
+  
+  int sample_i = 0;
+  int bsize = 8;
+  float main_x[128], main_y[128];
+  float sub_x[128], sub_y[128];
+  memset(sub_x, 0, sizeof(sub_x));
+  memset(sub_y, 0, sizeof(sub_y));
+  while (sample_i < w_in.num_frames)
+  {
+      for (int i = 0; i < bsize; i++)
+      {
+        float f[2];
+        wav_get_normalized(&w_in, sample_i + i, f);
+        main_x[i * 2] = f[0];
+        main_x[i * 2 + 1] = f[1];
+      }
+      MODFX_PROCESS(main_x, main_y, sub_x, sub_y, bsize);
+      for (int i = 0; i < bsize; i++)
+      {
+          wav_set_normalized(&w_out, sample_i, &main_y[i * 2]);
+          sample_i++;
+      }
+      bsize *= 2;
+      if (bsize > 64)
+          bsize = 8;
+  }
+  
+  if (write_wav_file(argv[2], &w_out) != 0)
+      puts("Writing output file failed");
+  free_wav_file(&w_in);
+  free_wav_file(&w_out);
+  return 0;
+}
---
+++ common/sound/usermodfx.h
@@ -0,0 +1,71 @@
+#pragma once
+
+#include <stdint.h>
+
+#include <math.h>
+typedef enum
+{
+    k_user_modfx_param_time = 0,
+    k_user_modfx_param_depth,
+    k_num_user_modfx_param_id
+} user_modfx_param_id_t;
+typedef struct user_osc_param
+{
+    int32_t shape_lfo;
+    uint16_t pitch;
+    uint16_t cutoff;
+    uint16_t resonance;
+    uint16_t reserved0[3];
+} user_osc_param_t;
+
+
+static inline float clipmaxf(const float x, const float m)
+{
+    return (((x) >= m) ? m : (x));
+}
+
+static inline float linintf(const float fr, const float x0, const float x1)
+{
+    return x0 + fr * (x1 - x0);
+}
+
+#define k_note_mod_fscale      (0.00392156862745098f)
+#define k_note_max_hz          (23679.643054f)
+#define k_samplerate        (48000)
+#define k_samplerate_recipf (2.08333333333333e-005f)
+
+
+static inline float param_val_to_f32(uint16_t v)
+{
+    return v / 1023.0f;
+}
+
+typedef int32_t q31_t;
+
+
+#define q15_to_f32_c 3.05175781250000e-005f
+#define q31_to_f32_c 4.65661287307739e-010f
+
+#define q15_to_f32(q) ((float)(q) * q15_to_f32_c)
+#define q31_to_f32(q) ((float)(q) * q31_to_f32_c)
+
+#define f32_to_q15(f)   ((q15_t)ssat((q31_t)((float)(f) * ((1<<15)-1)),16))
+#define f32_to_q31(f)   ((q31_t)((float)(f) * (float)0x7FFFFFFF))
+
+#define __fast_inline
+
+
+static inline float midi_note_to_increment(int note)
+{
+    return (float) (440.0 * pow(2, (note - 69) / 12.0)) / k_samplerate * 2;
+}
+
+static inline float osc_w0f_for_note(uint8_t note, uint8_t mod)
+{
+    const float f0 = midi_note_to_increment(note);
+    const float f1 = midi_note_to_increment(note + 1);
+
+    const float f = clipmaxf(linintf(mod * k_note_mod_fscale, f0, f1), k_note_max_hz);
+
+    return f;                   // f * k_samplerate_recipf;
+}
